a69b2cfc5246a2e5fff8f85632decee09e0189f4
aloksharma10 <aloksharma10969@gmail.com>
2025-01-03 05:29:57 +0530

    follow fallback error handle

--- src/workers/leadWorker.ts
+++ src/workers/leadWorker.ts
@@ -1089,7 +1089,7 @@ const updateLeadFinanceStatus = async (leadId: string, financeStatus: boolean, u
 
 const updateLeadFollowUpDate = async (feedback: any, leadId: string, nextFollowUpDate: string, remark: string, customerResponse: string, rating: string, memberName: string) => {
     try {
-        const feedbackData = feedback.map((fb: any) => ({
+        const feedbackData = feedback.length > 0 && feedback?.map((fb: any) => ({
             name: fb.name,
             value: fb.value,
             fieldType: (fb.fieldType as FieldType),
