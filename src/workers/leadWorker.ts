ef9b6607061b9f2117333865e61a882393f7f468
aloksharma10 <aloksharma10969@gmail.com>
2025-01-03 05:53:57 +0530

    update

--- src/workers/leadWorker.ts
+++ src/workers/leadWorker.ts
@@ -1089,7 +1089,7 @@ const updateLeadFinanceStatus = async (leadId: string, financeStatus: boolean, u
 
 const updateLeadFollowUpDate = async (feedback: any, leadId: string, nextFollowUpDate: string, remark: string, customerResponse: string, rating: string, memberName: string) => {
     try {
-        const feedbackData = feedback.length > 0 && feedback?.map((fb: any) => ({
+        const feedbackData = feedback?.length > 0 && feedback?.map((fb: any) => ({
             name: fb.name,
             value: fb.value,
             fieldType: (fb.fieldType as FieldType),
@@ -1122,7 +1122,7 @@ const updateLeadFollowUpDate = async (feedback: any, leadId: string, nextFollowU
 
 const updateProspectFollowUpDate = async (feedback: any, leadId: string, nextFollowUpDate: string, remark: string, customerResponse: string, rating: string, memberName: string) => {
     try {
-        const feedbackData = feedback.length > 0 && feedback?.map((fb: any) => ({
+        const feedbackData = feedback?.length > 0 && feedback?.map((fb: any) => ({
             name: fb.name,
             value: fb.value,
             fieldType: (fb.fieldType as FieldType),
